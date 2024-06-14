e6f6762930841b33d9d7a0702d246215ef1cb664
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2024-06-14 17:26:12 +0300

    added code by geeks for bubbleSort

--- src/javaDSA/BubbleSort.java
+++ src/javaDSA/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 		
 		for (int i=0; i<arr.length-1; i++) {
 			for(int j=0; j<arr.length-1-i; j++) {
-				if(arr[j+1] < arr[j]) {
+				if(arr[j] > arr[j+1]) {
 					int temp = arr[j];
 					arr[j] = arr[j+1];
 					arr[j+1] = temp;
@@ -20,8 +20,34 @@ public class BubbleSort {
 		System.out.println();
 	}
 	
+	public static void bubbleSortGeek(int arr[]) {
+		for (int i=0; i<arr.length-1; i++) {
+			boolean swapped = false;
+			for(int j=0; j<arr.length-1-i; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+					swapped = true;
+				}
+			}			
+			//if no elements were swapped
+			//by inner loop, then break
+			if(swapped == false) {
+				break;
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
 	public static void main (String args[]) {
-		int numbers[] = {5, 4, 1, 3, 2};
-		bubbleSort(numbers);
+		int numbers[] = {5, 4, 3, 2, 1};
+		int numbers2[] = { 64, 34, 25, 12, 22, 11, 90 };
+		//bubbleSort(numbers);
+		bubbleSortGeek(numbers);
 	}
 }
