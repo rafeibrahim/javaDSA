a75228cbc65593c29dc775148f3c41d5b1a3e3e5
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2024-06-14 17:26:12 +0300

    bubble sort code java

--- src/javaDSA/BubbleSort.java
+++ src/javaDSA/BubbleSort.java
@@ -2,7 +2,26 @@ package javaDSA;
 
 public class BubbleSort {
 	
+	public static void bubbleSort(int arr[]) {
+		
+		for (int i=0; i<arr.length-1; i++) {
+			for(int j=0; j<arr.length-1-i; j++) {
+				if(arr[j+1] < arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
 	public static void main (String args[]) {
-		System.out.println("Hello World!");
+		int numbers[] = {5, 4, 1, 3, 2};
+		bubbleSort(numbers);
 	}
 }
