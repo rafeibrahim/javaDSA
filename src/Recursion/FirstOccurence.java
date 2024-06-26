77a4ee20862a96e1cc7b6ec48e967e09c3d0dff8
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2024-06-26 14:21:07 +0300

    recursion firstOccurence Problem

---
+++ src/Recursion/FirstOccurence.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class FirstOccurence {
+	
+	public static int firstOccurence(int arr[], int key, int i) {
+		//Base  Case
+		if (i == arr.length) {
+			return -1;
+		}
+		
+		if (arr[i] == key) {
+			return  i;
+		}
+
+		return firstOccurence(arr, key, i+1);
+	}
+	
+	public static void main (String args[]) {
+		int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+		int arr1[] = {};
+		System.out.println(firstOccurence(arr, 2, 0));
+	}
+
+}
