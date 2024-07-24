e1d1ce049fcae9a52ab351f48b2239a3cbaa506e
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2024-07-24 16:17:28 +0300

    class created to learn syntax of LL in Java Collection Framework

---
+++ src/com/dsa/linkedlist/Classroom.java
@@ -0,0 +1,28 @@
+package com.dsa.linkedlist;
+import java.util.LinkedList;
+
+
+public class Classroom {
+	
+	public static void main (String args[]) {
+		
+		//create - objects int, float, boolean -> Integer, Float, Character
+		//cannot use primitive data type for creating LinkedList
+		
+		LinkedList<Integer> ll = new LinkedList<>();
+		
+		//add
+		ll.addLast(1);
+		ll.addLast(2);
+		ll.addFirst(0);
+		
+		//0->1->2
+		System.out.println(ll);
+		
+		//remove
+		ll.removeLast();
+		ll.removeFirst();
+		System.out.println(ll);
+	}
+
+}
